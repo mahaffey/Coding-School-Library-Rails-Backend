89f0bcea51473b7a1c23eff97325ce0e2ff1d32f
drzl777 <andrew.zheng725@gmail.com>
2017-06-23 00:40:01 -0400

    added controller action for creating content

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -15,8 +15,24 @@ class Api::V1::ContentsController < ApplicationController
     render json: @content, status: 200
   end
 
+  def create
+    @content = Content.new(content_params)
+    if @content.save
+      render json: @content, status: 201
+    else
+      render json: {message: @content.errors.full_messages}, status: 500
+    end
+  end
 
+  private
 
+    def content_params
+      params.require(:content).permit(:title, :pic_url, :link_url, :description, :difficulty, :subtopic_id)
+    end
+
+    # def set_subtopic
+    #   @subtopic = Subtopic.find(params[:id])
+    # end
 
 
 end
